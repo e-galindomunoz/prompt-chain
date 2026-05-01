2f7647dd7ce38587099713f8c9d45e2b2538589d
Eduardo Galindo <20edd49@Eduardos-MacBook-Pro-7.local>
2026-05-01 13:19:42 -0400

    oauth bug fix

--- humorfl/app/auth/callback/route.ts
+++ humorfl/app/auth/callback/route.ts
@@ -1,29 +1,33 @@
 import { createServerClient } from '@supabase/ssr'
-import { cookies } from 'next/headers'
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
-export async function GET(request: Request) {
+export async function GET(request: NextRequest) {
   const { searchParams, origin } = new URL(request.url)
   const code = searchParams.get('code')
+
   if (code) {
-    const cookieStore = await cookies()
+    const response = NextResponse.redirect(`${origin}/dashboard`)
     const supabase = createServerClient(
       process.env.NEXT_PUBLIC_SUPABASE_URL!,
       process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
       {
         cookies: {
           getAll() {
-            return cookieStore.getAll()
+            return request.cookies.getAll()
           },
           setAll(cookiesToSet) {
             cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.set(name, value, options)
+              response.cookies.set(name, value, options)
             )
           },
         },
       }
     )
-    await supabase.auth.exchangeCodeForSession(code)
+    const { error } = await supabase.auth.exchangeCodeForSession(code)
+    if (!error) {
+      return response
+    }
   }
-  return NextResponse.redirect(`${origin}/dashboard`)
+
+  return NextResponse.redirect(`${origin}/login`)
 }
